878a69f2079499d1bc8f0c50e58fbf0b4689d8e9
yuto0109mcu <yuto0109mcu@gmail.com>
2020-11-20 21:22:56 +0900

    rebuild basic functions with next js

--- js/_calculation.js
+++
@@ -1,9 +0,0 @@
-const yes = document.querySelectorAll(".yes")
-const no = document.querySelectorAll(".no")
-
-let calcResult = 0
-const calculate = () => {
-   calcResult += 1
-}
-
-export { yes, no, calcResult, calculate }
--- js/_doAgain.js
+++
@@ -1,9 +0,0 @@
-const again = document.querySelector(".again")
-const reload = () => {
-   window.location.reload();
-}
-const showAgain = () => {
-   again.classList.remove("hide")
-}
-
-export { again, reload, showAgain }
